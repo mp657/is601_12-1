999a9a777c247d286da481a2100a61c3fe95a566
monika pattanayak <mp657@njit.edu>
2017-07-24 18:44:31 -0400

    added code for end and delete session cookie

--- index.php
+++ index.php
@@ -52,5 +52,20 @@ switch($action) {
         unset($_SESSION['cart12']);
         include('cart_view.php');
         break;
+    case 'end_session':
+        $_SESSION = array();
+	session_destroy();
+
+	$name = session_name();                // Get name of the session cookie
+	$expire = strtotime('-1 year');        // Create expiration date in the past
+	$params = session_get_cookie_params(); // Get session params
+	$path = $params['path'];
+	$domain = $params['domain'];
+	$secure = $params['secure'];
+	$httponly = $params['httponly'];
+	setcookie($name, '', $expire, $path, $domain, $secure, $httponly);
+
+	include('cart_view.php');
+	break;
 }
 ?>
